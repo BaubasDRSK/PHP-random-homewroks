f02dc99aab03decc91a7dd33c8b7590c35796526
BaubasDRSK <127085633+BaubasDRSK@users.noreply.github.com>
2023-05-29 11:29:45 +0300

    ciklai2 massyvai iki 9

--- ciklai2.php
+++ ciklai2.php
@@ -49,6 +49,7 @@ foreach ($arr as $a){
     echo implode(" ",$a);
     echo "<br>";
 }
+
 $sumuMasyvas = [];
 foreach ($arr as $ar){
     $sum = 0;
@@ -59,3 +60,133 @@ foreach ($arr as $ar){
 }
 
 echo "Sumos lygu: ".implode(" ", $sumuMasyvas);
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+$nArray = [];
+$temp = [];
+for ($i = 1; $i <=10; $i++){
+    $kiekis = rand(2,20);
+    for ($e = 1; $e<= $kiekis; $e++){
+        $temp[] = chr(rand(65,90));
+    }
+    $nArray[] = $temp;
+    $temp = [];
+}
+
+foreach ($nArray as &$aa){
+    arsort($aa);
+    $aa = array_reverse($aa);
+}
+unset($aa);
+
+foreach ($nArray as $a){
+    echo implode(" ",$a);
+    echo "<br>";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$nnArray = [];
+
+foreach($nArray as $aa){
+    $ilgis = count($aa);
+    if (!in_array("K", $aa)){
+        $ilgis += 20;
+    }
+    $nnArray[$ilgis] =$aa;
+}
+
+ksort($nnArray);
+
+foreach ($nnArray as $a){
+    echo implode(" ",$a);
+    echo "<br>";
+}
+
+
+//55555
+
+echo "<br>";
+echo "<br>";
+
+$nnnArr = [];
+$usrId = [];
+
+for ($i=1; $i<= 30; $i++){
+    do {
+        $id = rand(1,1000000);
+    } while (in_array($id, $usrId));
+    $usrId[] = $id;
+    $temp["user_id"]=$id;
+    $temp["place_in_row"]=rand(0,100);
+    $nnnArr[]=$temp;
+}
+foreach ($nnnArr as $a){
+    echo http_build_query($a,''," / ");
+    echo "<br>";
+}
+
+///66666
+
+
+
+$user_id  = array_column($nnnArr, 'user_id');
+
+// Sort the data with volume descending, edition ascending
+// Add $data as the last parameter, to sort by the common key
+array_multisort($user_id, SORT_ASC, $nnnArr);
+
+echo"<br>";
+foreach ($nnnArr as $a){
+    echo http_build_query($a,''," / ");
+    echo "<br>";
+}
+
+$place_in_row = array_column($nnnArr, 'place_in_row');
+array_multisort($place_in_row, SORT_DESC, $nnnArr);
+
+echo"<br>";
+foreach ($nnnArr as $a){
+    echo http_build_query($a,''," / ");
+    echo "<br>";
+}
+
+////888888
+$arr8 =[];
+$arr8_2 = [];
+for ($i=1; $i <=10; $i++){
+    $x = rand (0, 5);
+    if ($x === 0){
+        $arr8[] = rand(0,10);
+    } else {
+        for ($e=1; $e<=$x; $e++){
+            $arr8_2[]=rand(0,10);
+        }
+        $arr8[] = $arr8_2;
+        $arr8_2 = [];
+    }
+}
+echo"<pre>";
+var_dump($arr8);
+
+//// 9999
+$sums = [0];
+foreach ($arr8 as $aaa){
+    if (gettype($aaa) === "integer") {
+        $sums[0]  += $aaa;
+    } else {
+        $sums[] = array_sum($aaa);
+    }
+}
+
+echo "<br>";
+echo implode(" ", $sums);
+
+sort($sums);
+
+echo "<br>";
+echo implode(" ", $sums);
